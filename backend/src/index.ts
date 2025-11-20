3528546d2fdcdb87466bdb6ce009373d14aaa999
shi code <kdshivayk@gmail.com>
2025-11-20 17:22:01 +0530

    added /generate-otp end point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log( "this si me ")
+import expres from 'express'
+
+ const app = expres ();
+
+app.use(expres.json());
+
+const otpStore :Record<string, string> = {}
+
+app.post ( "/generate-otp", (req, res )=>{
+    const email = req.body.email;
+
+    if (!email){
+         return res.status(400).json ({ message: "email is not found"})
+    }
+})
